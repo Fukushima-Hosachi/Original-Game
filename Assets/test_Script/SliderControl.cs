3a2ba8efbe4adaa7c9d5237d413bbe8009f528e6
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-01-15 20:29:29 +0900

    スライダーの数値取得
    スライダーの数値の取得
    次はその数値をRGBとして受け渡し色を変える

---
+++ Assets/test_Script/SliderControl.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SliderControl : MonoBehaviour
+{
+    public Slider slider;
+    public float RGB;
+
+    //スライダーの値を「取得する」ためのパブリック変数を定義する関数
+    //取得した値のCubeの回転への反映もここで行います。
+    public void SliderDemo_CubeRot(float newValue)
+    {
+        // キューブを回転させるための変数を宣言
+        // Vector3変数は3つの値を格納できる型
+        // その変数に現在のオイラー角xyzを代入
+        // thisはこのスクリプトをアタッチしたGameObjectを表す
+        Vector3 rot = this.transform.localEulerAngles;
+
+        // 縦軸のy成分を代入
+        rot.x = newValue;
+
+        // 新しい数値をキューブの角度に代入
+        this.transform.localEulerAngles = rot;
+    }
+
+
+
+
+ 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        slider = GameObject.Find("Slider").GetComponent<Slider>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RGB = slider.value;
+        Debug.Log(RGB);
+        
+    }
+
+
+    public void SliderDemo_ClorChange(float newValue)
+    {
+        //スライダーで色を変えたい
+        //RGB
+
+        //R,G,Bの各値を0～255でスライダーで調整
+        //スライダーの値をRGBに送る
+    }
+
+}
--- Assets/test_Script/TextController.cs
+++ Assets/test_Script/TextController.cs
@@ -16,7 +16,8 @@ public class TextController : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+     
         //\n‚Å‰üs
         textlabel.text = charaname + "\n" + word[ num ] ;
         
