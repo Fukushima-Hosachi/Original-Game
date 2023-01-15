0cd85bacc9a98cca8df98e80f304e9db88721d88
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-01-15 21:30:37 +0900

    RGBスライドバーで変えられるようになった

--- Assets/test_Script/SliderControl.cs
+++ Assets/test_Script/SliderControl.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 
 public class SliderControl : MonoBehaviour
 {
-    public Slider slider;
-    public float RGB;
+    public Slider R_slider, G_slider, B_slider;
+    //public float RGB;
+    public float R;
+    public float G;
+    public float B;
     GameObject Square;
 
     //スライダーの値を「取得する」ためのパブリック変数を定義する関数
@@ -38,7 +41,10 @@ public class SliderControl : MonoBehaviour
     void Start()
     {
 
-        slider = GameObject.Find("Slider").GetComponent<Slider>();
+        R_slider = GameObject.Find("Slide_R").GetComponent<Slider>();
+        G_slider = GameObject.Find("Slide_G").GetComponent<Slider>();
+        B_slider = GameObject.Find("Slide_B").GetComponent<Slider>();
+        float R, G, B = 0;
         gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
         
     }
@@ -46,22 +52,53 @@ public class SliderControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RGB = slider.value;
-        Debug.Log(RGB);
-        
+        R = R_slider.value;
+        G = G_slider.value;
+        B = B_slider.value;
+
+        gameObject.GetComponent<Renderer>().material.color = new Color(R, G, B);
+
     }
 
 
-    public void SliderDemo_ClorChange(float newValue)
+    public void SliderDemo_ColorChangeR(float newValue)
     {
         //スライダーで色を変えたい
         //RGB
 
         //R,G,Bの各値を0～255でスライダーで調整
         //スライダーの値をRGBに送る
+        Debug.Log("R=" + R);
 
-        gameObject.GetComponent<Renderer>().material.color = new Color(RGB , RGB, RGB);
+        //gameObject.GetComponent<Renderer>().material.color = new Color(R , R, R);
 
     }
+
+    public void SliderDemo_ColorChangeG(float newValue)
+    {
+        //スライダーで色を変えたい
+        //RGB
+
+        //R,G,Bの各値を0～255でスライダーで調整
+        //スライダーの値をRGBに送る
+        Debug.Log("G=" + G);
+
+        //gameObject.GetComponent<Renderer>().material.color = new Color(R , R, R);
+
+    }
+
+    public void SliderDemo_ColorChangeB(float newValue)
+    {
+        //スライダーで色を変えたい
+        //RGB
+
+        //R,G,Bの各値を0～255でスライダーで調整
+        //スライダーの値をRGBに送る
+        Debug.Log("B=" + B);
+
+        //gameObject.GetComponent<Renderer>().material.color = new Color(R , R, R);
+
+    }
+
 
 }
